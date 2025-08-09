c81d9b66e95e27093e01b5fd4a88fefcb7e2b8c4
WGabriel <gwerminski@student.agh.edu.pl>
2025-08-09 23:31:12 +0200

    0329-LONGEST-INCREASING-PATH-IN-A-MATRIX Stats: Time: 160 ms (54.18%), Space: 24.4 MB (9.66%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -13,32 +13,23 @@ class Solution:
                 return memo[(i, j )]
 
             nonlocal m , n
-            flag = False
             res = 1
 
             new_i , new_j = i , j+1 # right
             if new_j < m and check_matrix(i,j,new_i,new_j) :
                 res = rec( new_i , new_j ) + 1
-                flag = True
 
             new_j = j - 1 # left
             if new_j >= 0 and check_matrix(i,j,new_i,new_j) :
                 res = max( res , rec( new_i, new_j ) + 1 )
-                flag = True
-
+        
             new_i , new_j = i-1 , j # top
             if new_i >= 0 and check_matrix(i,j,new_i,new_j) :
                 res =max( res ,  rec( new_i, new_j ) + 1 )
-                flag = True
-
+                
             new_i = i+1 # bottom
             if new_i < n and check_matrix(i,j,new_i,new_j) :
                 res = max( res , rec( new_i, new_j ) + 1 )
-                flag = True
-
-            if not flag :
-                memo[ (i,j) ] = 1
-                return 1
 
             memo[ (i,j) ] = res
             return res
