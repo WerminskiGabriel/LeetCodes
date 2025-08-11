b72d0d757bb2ec2b09379ab817059652e09af131
WGabriel <gwerminski@student.agh.edu.pl>
2025-08-11 21:07:46 +0200

    0139-WORD-BREAK Stats: Time: 0 ms (100%), Space: 17.7 MB (97.66%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,31 +1,15 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         
-        memo = {}
+        n = len( s )
+        wd_len = max( map( len , wordDict ) )
+        dp = [False] * ( n+1 )
+        dp[0] = True
+        wordDict = dict.fromkeys( wordDict , 0 )
 
-        def rec( s_idx , i ) :
-            nonlocal s , wordDict
-
-            if (s_idx, i ) in memo :
-                return memo[ (s_idx ,i ) ]
-
-            if s_idx < 0 or i < 0 or i >= len( wordDict ) :
-                return False
-
-            if s[ :s_idx+1] == wordDict[i] :
-                return True
-
-            f_idx = s_idx - len( wordDict[i] ) + 1
-            res = False
-
-            if s[ f_idx : s_idx+1 ] == wordDict[i] :
-                res = rec( f_idx-1 , i ) or rec( f_idx-1 , i-1 )
-                #for j in range( i+1 , len( wordDict ) ) :
-                res = res or rec( f_idx-1 , len(wordDict)-1 )
-
-            res = res or rec( s_idx , i-1 )
-
-            memo[ ( s_idx , i ) ] = res
-            return res
-
-        return  rec( len( s )-1 , len( wordDict )-1 )
+        for i in range( 1 ,  n+1 ) :
+            for j in range( i-1 , max( -1 , i-wd_len-1 ) , -1 ) : 
+                if dp[j] and s[j:i] in wordDict :
+                    dp[i] = True
+                    break
+        return dp[n]
