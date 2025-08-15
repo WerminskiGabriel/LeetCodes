9321f313dbb57470f71aec43d1c4e7ed88deed6c
WGabriel <gwerminski@student.agh.edu.pl>
2025-08-15 22:47:58 +0200

    0016-3SUM-CLOSEST Stats: Time: 295 ms (89.17%), Space: 17.7 MB (78.45%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -3,29 +3,6 @@ class Solution:
         nums.sort()
         n = len( nums )
         res = float( "inf" )
-
-        for i in range( n -2 ) :
-            j = i+1
-            k = n-1
-            while j < k :
-                sum = nums[i] + nums[j] + nums[k]
-                if abs(res-target) > abs( sum - target ) :
-                    res = sum
-                if sum == target :
-                    return sum
-
-                elif sum > target :
-                    k -= 1
-                else :
-                    j += 1
-        return res
-
-
-"""
-        
-        nums.sort()
-        n = len( nums )
-        res = float( "inf" )
         val = 0
 
         for i in range( n -2 ) :
@@ -37,12 +14,11 @@ class Solution:
                 if res > dist :
                     res = dist
                     val = sum
-                res = min( res,  dist )
                 if sum == target :
-                    return 0
+                    return sum
 
-                elif sum > 0 :
+                elif sum > target :
                     k -= 1
                 else :
                     j += 1
-        return val"""
+        return val
