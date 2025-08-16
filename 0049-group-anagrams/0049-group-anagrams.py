b64052bda97616c3de7cffd734792c658b859a62
WGabriel <gwerminski@student.agh.edu.pl>
2025-08-16 18:43:48 +0200

    0049-GROUP-ANAGRAMS Stats: Time: 15 ms (48.93%), Space: 21.8 MB (36.56%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -4,22 +4,16 @@ class Solution:
         n = len( strs )
 
         for i in range( n ) :
-            strs[i] = ( sorted( strs[i] ) , i )
+            strs[i] = ( "".join(sorted( strs[i] ) ) , i )
         strs.sort()
-        result = []
+        result = {}
 
-        current = strs[0][0]
-        start = 0
-        for i in range( n ) :
-            if current != strs[i][0] :
-                res = []
-                for j in range( start , i ) :
-                    res.append( original[ strs[j][1]] )
-                result.append( res.copy() )
-                start = i
-                current = strs[i][0]
-        res = []
-        for j in range( start , n ) :
-            res.append( original[ strs[j][1] ] )
-        result.append( res )
-        return result
+        for i in range( n ) : #in strs
+            st , idx = strs[i][0] , strs[i][1]
+            orig = original[ idx ]
+            
+            if st in result :
+                result[st].append( orig )
+            else :
+                result[ st ] = [orig]
+        return list( result.values() )
