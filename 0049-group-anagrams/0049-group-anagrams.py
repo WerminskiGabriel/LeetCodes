63efb49d4792e71e4597160930ae35a50e0c3b7a
WGabriel <gwerminski@student.agh.edu.pl>
2025-08-16 18:27:35 +0200

    0049-GROUP-ANAGRAMS Stats: Time: 23 ms (17.84%), Space: 22.2 MB (35.43%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,23 +2,11 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         original = strs.copy()
         n = len( strs )
-        def hash( str ) :
-        #   poynomaial rolling hash algo :
-            res = 0
-            m = 1e9+7
-            power = 1
-            q = 31
-
-            for i in range( len( str ) ) :
-                res = ( res + ord( str[i] ) * power ) % m
-                power = ( power * q ) % m
-            return int( res )
 
         for i in range( n ) :
             strs[i] = ( sorted( strs[i] ) , i )
         strs.sort()
         result = []
-        strs = [ ( hash( str[0] ) , str[1] ) for str in strs ]
 
         current = strs[0][0]
         start = 0
@@ -34,5 +22,4 @@ class Solution:
         for j in range( start , n ) :
             res.append( original[ strs[j][1] ] )
         result.append( res )
-
         return result
