fe0fb9c978748373caf54f8e57aaf65a72b8c4d2
WGabriel <gwerminski@student.agh.edu.pl>
2025-08-17 10:16:15 +0200

    0005-LONGEST-PALINDROMIC-SUBSTRING Stats: Time: 342 ms (45.42%), Space: 18 MB (25.81%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,35 +1,23 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-       
-        def odd( i , left , right ) :
-        #	left = right = i
+       	def odd( i , left , right ) :
             res = 0
-            s_res = []
             while left >= 0 and right < len( s ) :
                 if s[left] == s[right] :
                     leng = right - left + 1
                     if leng > res :
-                        s_res.append( s[right] )
                         res = leng
                 else :
                     break
                 left -= 1
                 right += 1
-            return s_res
+            return res
 
-        max_even = max_odd = []
+        start = 0
+        end = 0
         for i in range( len( s ) ) :
-
-            new = odd( i,i,i )
-            if len( max_odd ) < len( new ) :
-                max_odd = new
-
-            new = odd( i, i, i+1 )
-            if len( max_even ) < len( new ) :
-                max_even = new
-
-        max_even = "".join(  max_even[::-1] ) + "".join( max_even )
-        max_odd = "".join(max_odd[len(max_odd)-1:0:-1])  +max_odd[0] +"".join(max_odd[1:])
-        if len( max_odd ) > len( max_even ) :
-            return max_odd
-        return max_even 
+            res = max( odd( i,i,i) , odd( i, i, i+1 ) )
+            if end - start < res :
+                start = i-(res-1)//2
+                end = i+res//2
+        return s[start:end+1]
