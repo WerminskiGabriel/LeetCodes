f74d67d67f8b84a2e8eb241acc0d4d7c11cc1f43
WGabriel <gwerminski@student.agh.edu.pl>
2025-08-21 11:25:15 +0200

    1402-COUNT-SQUARE-SUBMATRICES-WITH-ALL-ONES Stats: Time: 44 ms (97.21%), Space: 20.2 MB (90.78%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.py
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.py
@@ -8,17 +8,10 @@ class Solution:
         for i in range( 1 , m ) :
             res += matrix[i][0]
 
-        def check_square( i , j ,  m ) :
-            return matrix[i][j]>=m and matrix[i-1][j-1]>=m and matrix[i-1][j]>=m and matrix[i][j-1]>=m
-
         for i in range( 1 , m ) :
             for j in range( 1 , n ) :
-                for k in range( 1 , n ) :
-                    if i >= k and j >= k and check_square( i , j , k ) :
-                        matrix[i][j] += 1
-                    else :
-                        break
-
-                res += matrix[i][j]
+                if matrix[i][j] != 0 : 
+                    matrix[i][j] = 1 + min( matrix[i][j-1] , matrix[i-1][j-1] , matrix[i-1][j] )
+                    res += matrix[i][j]
 
         return res
