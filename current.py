35b4f4d4653f9b753d0b754d999842933d26dfd1
WGabrielCode <gwerminski@student.agh.edu.pl>
2025-08-22 21:38:05 +0200

    current problem

--- current.py
+++ current.py
@@ -1,22 +1,8 @@
-def func( s , wordDict ) :
+def func( mat ) :
+	
 
-	n = len( s )
-	#o
-	wd_len = max( map( len , wordDict ) )
-	dp = [False] * ( n+1 )
-	dp[0] = True
-	wordDict = dict.fromkeys( wordDict , 0 )
-	print( wordDict )
-	for i in range( 1 ,  n+1 ) :
-		for j in range( i-1 , max( -1 , i-wd_len-1 ) , -1 ) :
-			if dp[j] and s[j:i] in wordDict :
-				dp[i] = True
-				break
+matrix =[[0,1,1,0],
+         [0,1,1,1],
+         [1,1,1,0]]
 
-	return dp[n]
-
-s = "leetcodecode" ,       "applepenapple",     "dogs" ,             "applepie"
-wordDict = ["leet","code"] , ["apple","pen"] , ["dog","s","gs"] ,["pie","pear","apple","peach"]
-
-idx = 1
-print( func( s[idx] , wordDict[idx] ) )
+print( func( matrix ) )
