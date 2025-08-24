3deb142da63af2fc019aa8e51e3f3dc78500c139
WGabriel <gwerminski@student.agh.edu.pl>
2025-08-24 18:10:13 +0200

    1129-LONGEST-STRING-CHAIN Stats: Time: 48 ms (77.18%), Space: 18.3 MB (31.55%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.py
+++ 1129-longest-string-chain/1129-longest-string-chain.py
@@ -1,41 +1,15 @@
 class Solution:
     def longestStrChain(self, words: List[str]) -> int:
-        n =  len( words )
-        words = sorted( words , key = lambda x : len( x ) )
-        
-        memo = {}
-        def is_predecessor( pre , par ) :
-            if (pre,par) in memo:
-                return memo[ (pre,par) ]
+        n = len( words )
+        words = sorted( words, key = lambda x : len( x ) )
+        memo = { }
+        res = 1
+        for word in words :
+            memo[word] = 1
 
-            lena = len( pre )
-            lenb = len( par )
-
-            if lena + 1 != lenb :
-                memo[ (pre,par) ] = False 
-                return False
-
-            for i in range( lena ) :
-                a = par[:i]+par[i+1:]
-                if pre == a :
-                    memo[ (pre,par) ] = True
-                    return True
-            a = par[:lenb-1]
-            memo[ (pre,par) ] = (pre == a)
-            return memo[ (pre,par) ]
-
-        cnt = 1
-        res = 0
-        dp = [1] * n
-        max_val = 1
-        for i in range( 1, n ) :
-            for j in range( i-1 , -1 , -1 ) :
-                if len( words[j] ) == len( words[i] ) :
-                    continue
-                if len( words[j] ) != len( words[i] )-1 :
-                    break
-                if is_predecessor( words[j] , words[i] ) :
-                    dp[i] = max( dp[i] , dp[j]+1 )
-                    max_val = max( max_val , dp[i] )
-
-        return max_val
+            for i in range( len(word) ) :
+                pre = word[:i] + word[i+1:]
+                if pre in memo :
+                    memo[word] = max( memo[word] , memo[pre] +1 )
+                    res = max( res , memo[word] )
+        return res
