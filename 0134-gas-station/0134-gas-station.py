da51b2e14aa41be85026c4bb0cae52997e41d625
WGabriel <gwerminski@student.agh.edu.pl>
2025-08-25 14:33:54 +0200

    0134-GAS-STATION Stats: Time: 35 ms (30.6%), Space: 23.9 MB (9.89%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -3,24 +3,15 @@ class Solution:
         n = len( gas )
         gas = [ gas[i]-cost[i] for i in range( n ) ]
 
+        if sum( gas ) < 0 : 
+                    return -1
+
         start = 0
         total = 0
-        i = 0
-        looped = False
-        while i <= n :
-            if i == n  :
-                if looped :
-                    break
-                i = 0
-                looped = True
+        for i in range( n ) :
+            total += gas[i] 
             if total < 0 :
-                if looped :
-                    break
-                start = i
-                total = gas[i]
-            else :
-                total += gas[i]
-            i += 1
-            if i == start :
-                break
-        return start if total >= 0 else -1
+                total = 0 
+                start = i+1
+        return start
+    
