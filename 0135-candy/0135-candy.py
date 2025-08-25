e91fa01a565b8bb4ff34b142f88adf7773e999d6
WGabriel <gwerminski@student.agh.edu.pl>
2025-08-25 15:53:13 +0200

    0135-CANDY Stats: Time: 19 ms (41.78%), Space: 19.9 MB (34.96%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -2,17 +2,13 @@ class Solution:
     def candy(self, ratings: List[int]) -> int:
         n = len( ratings )
 
-        organized = sorted( [ (ratings[i] , i ) for i in range( n ) ] )
+        candies = [1] * n
 
-        total = 0
-        dp = [1] * n
         for i in range( 1 , n ) :
-            idx = organized[i][1]
-            left = idx-1
-            right = idx+1
-            if idx-1 >= 0 and ratings[ idx-1 ] < ratings[idx] :
-                dp[idx] = max( dp[idx] , dp[idx-1] + 1 )
-            if idx+1 < n and ratings[ idx + 1 ] < ratings[ idx ] :
-                dp[ idx ] = max( dp[ idx ], dp[ idx + 1 ] + 1 )
-        print( dp )
-        return sum( dp )
+            if ratings[i-1] < ratings[i] :
+                candies[i] = candies[i-1] + 1
+
+        for i in range( n-2 , -1 , -1 ) :
+            if ratings[i+1] < ratings[i] :
+                candies[i] = max( candies[i] , candies[i+1] + 1 )
+        return sum( candies ) 
