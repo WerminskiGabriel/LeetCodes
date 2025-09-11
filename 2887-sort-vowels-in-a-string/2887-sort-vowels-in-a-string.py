ec0d1a3b1f64c336a69a86f0b2157737c5e8a330
WGabriel <gwerminski@student.agh.edu.pl>
2025-09-11 14:03:46 +0200

    2887-SORT-VOWELS-IN-A-STRING Stats: Time: 67 ms (91.12%), Space: 20.4 MB (65.37%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.py
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.py
@@ -1,27 +1,16 @@
 class Solution:
     def sortVowels(self, s: str) -> str:
-        vowels = {
-        'a': True, 'e': True, 'i': True, 'o': True, 'u': True,
-        'A': True, 'E': True, 'I': True, 'O': True, 'U': True }
-
-        found_vowels = []
-        is_vowel = [False] * len( s )
-
-        for i , char in enumerate( s ) :
-            if char in vowels :
-                found_vowels.append( char ) 
-                is_vowel[i] = True
-
         
-        found_vowels.sort(reverse=True)
-        i = 0 
+        vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
 
-        result = ""
-        for i in range( len( s ) ) :
-            if is_vowel[i] :
-                result += ( found_vowels.pop() )
+        found_vowels = sorted( [ char for char in s if char in vowels ] , reverse = True )
+
+        result = [ ]
+
+        for char in s  :
+            if char in vowels:
+                result.append( found_vowels.pop() )
             else :
-                result += ( s[i] )
+                result.append( char )
 
-        return result     
-
+        return "".join(result)
