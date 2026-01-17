a3d18c636a115b44dbe5d465f1da2e9119d63225
Werminski_Gabriel <gwerminski@student.agh.edu.pl>
2026-01-17 17:37:42 +0100

    1046-MAX-CONSECUTIVE-ONES-III Stats: Time: 56 ms (86.62%), Space: 22.3 MB (9.87%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
@@ -1,40 +1,40 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        from collections import deque
-
-        que = deque( )
         length = len( nums )
         i = 0
         max_value = 0
         current_value = 0
 
-        start = 0
-        while i < length and nums[i] == 0 :
-
-            if k <= 0 :
-                i += 1
-                continue
-            if k > len( que ) :
-                current_value += 1
-                que.append( i )
-            elif k <= len( que ) :
-                last_i = que.popleft( )
-                start = que[0] if que else last_i + 1
-                que.append( i )
-
-            i += 1
-        print( que , i , current_value )
-        while i < length :
-            if nums[i] == 0 :
-                if len( que ) < k :
-                    que.append( i )
+        if k == 0 :
+            while i < length :
+                if nums[i] == 1 :
                     current_value += 1
                 else :
                     max_value = max( max_value , current_value )
-                    if k == 0 :
-                        start = i + 1
-                        current_value = 0
+                    current_value = 0
+                i += 1
+        else :
+            from collections import deque
+            start = 0
+            que = deque( )
+            while i < length and nums[i] == 0 :
+                if k > len( que ) :
+                    current_value += 1
+                    que.append( i )
+                elif k <= len( que ) :
+                    last_i = que.popleft( )
+                    start = que[0] if que else last_i + 1
+                    que.append( i )
+
+                i += 1
+
+            while i < length :
+                if nums[i] == 0 :
+                    if len( que ) < k :
+                        que.append( i )
+                        current_value += 1
                     else :
+                        max_value = max( max_value , current_value )
                         if nums[que[0] - 1] == 1 :
                             current_value -= (que[0] - start)
                             start = que.popleft() + 1
@@ -42,8 +42,8 @@ class Solution:
                         else :
                             start = que.popleft( ) + 1
                             que.append( i )
-            else :
-                current_value += 1
-            i += 1
+                else :
+                    current_value += 1
+                i += 1
 
         return max( current_value , max_value )
